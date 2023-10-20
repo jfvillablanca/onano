1816d548bc23198e81545d9514b513232f6698da
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-20 14:45:01 +0800

    feat: vertical scroll with Lft@start and Rgt@end

--- src/editor.rs
+++ src/editor.rs
@@ -157,10 +157,24 @@ impl Editor {
                     y = y.saturating_add(1);
                 }
             }
-            Key::Left => x = x.saturating_sub(1),
+            Key::Left => {
+                if x > 0 {
+                    x -= 1;
+                } else if y > 0 {
+                    y -= 1;
+                    if let Some(row) = self.document.get_row(y) {
+                        x = row.len();
+                    } else {
+                        x = 0;
+                    }
+                }
+            }
             Key::Right => {
                 if x < width {
-                    x = x.saturating_add(1);
+                    x += 1;
+                } else if y < height {
+                    x = 0;
+                    y += 1;
                 }
             }
             Key::PageUp => {
